cc0c057759ce579c118735324def02bd8f05f330
Nochang0 <hy040504@kakao.com>
2026-03-22 16:58:59 +0900

    (2026-03-22 16:58:59) 업데이트 완료

--- git.sh
+++ git.sh
@@ -4,7 +4,7 @@ read -p "(2Grade) 커밋 메시지: " msg
 
 # 입력이 비었으면 기본 메시지 사용
 if [ -z "$msg" ]; then
-    msg="$(date '+%Y-%m-%d %H:%M:%S') 업데이트 완료"
+    msg="($(date '+%Y-%m-%d %H:%M:%S')) 업데이트 완료"
 
 fi
 
